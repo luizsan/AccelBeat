1d59acf628a8ec021932162708cafa46458f96e1
luizsan <luizsanq@gmail.com>
2022-03-06 21:55:02 -0300

    Add editor UI fonts

--- Scripts/05 Fonts.lua
+++ Scripts/05 Fonts.lua
@@ -1,7 +1,12 @@
 Font = {
     System = "Common Normal",
     Combo = "Combo Numbers",
+
     UIHeavy = "NewRodinEB-24",
     UINormal = "NewRodinB-24",
+    
+    EditorUIHeavy = "RodinEB-20",
+    EditorUINormal = "RodinDB-20",
+
     LargeNumbers = "NewRodinEB-64-Numbers",
 }
